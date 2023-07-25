1bb076e38e4c1e87473ffcc5c87af01b314c2662
Виноградов Леонид <vinogradovls@yandex.ru>
2023-07-26 02:57:02 +0300

    final2

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404
 from django.db.models import Count, Q
-from django.http import Http404
 from django.urls import reverse, reverse_lazy
 from django.utils import timezone
 from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
